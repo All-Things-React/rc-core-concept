cf97ecaa21ebff3d9d38cb540c5d4516481f53bc
deepak <deepakkumardash313@gmail.com>
2022-12-11 01:05:21 +0530

    add : vite, base files & config

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement('div', null, 'Hello World!'))
