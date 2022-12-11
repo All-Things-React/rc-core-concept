c97ba6fd96172f58a533d874a04235fa5e71dec8
deepak <deepakkumardash313@gmail.com>
2022-12-11 20:42:54 +0530

    fetch data from apis

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Pet from './Pet'
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 
 const SearchParams = () => {
   const [location, setLocation] = useState('')
   const [animal, setAnimal] = useState('')
   const [breed, setBreed] = useState('')
+  const [pets, setPets] = useState([])
   const breeds = []
+
+  const requestPets = async () => {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
+
+    const json = await res.json()
+    setPets(json.pets)
+  }
+
+  useEffect(() => {
+    requestPets()
+  }, [])
 
   return (
     <div className='search-params'>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          requestPets()
+        }}
+      >
         <label htmlFor='location'>
           Location
           <input id='location' value={location} onChange={e => setLocation(e.target.value)} placeholder='Location' />
@@ -50,6 +68,9 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      {pets.map(pet => (
+        <Pet key={pet.id} name={pet.name} animal={pet.animal} breed={pet.breed} />
+      ))}
     </div>
   )
 }
